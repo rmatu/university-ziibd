a55247ba66581dbc476758aa192a6a18175eb14b
Ryszard Matula <56454967+rmatu@users.noreply.github.com>
2020-11-09 20:13:11 +0100

    Update SQL-3.sql

--- university-ziibd/QUERIES/SQL-3.sql
+++ university-ziibd/QUERIES/SQL-3.sql
@@ -1,16 +1,16 @@
 
-====1====
+--====1====
 
 SELECT sysdate "DATE"
 FROM dual;
 
-====2=====
+--====2=====
 
 SELECT employee_id, last_name, salary,
 ROUND(salary * 1.115, 0) "New Salary"
 FROM employees
 
-====4=====
+--====4=====
 
 SELECT employee_id, last_name, salary,
 ROUND(salary * 1.115, 0) "New Salary",
@@ -18,7 +18,7 @@ ROUND(salary * 1.115, 0) - salary "Increase"
 FROM employees
 
 
-=====5=====
+--=====5=====
 
 SELECT INITCAP(last_name), LENGTH(last_name)
 FROM employees
@@ -27,7 +27,7 @@ OR last_name LIKE 'M%'
 OR last_name LIKE 'A%'
 ORDER BY last_name
 
-====5.1=====
+--====5.1=====
 
 SELECT INITCAP(last_name), LENGTH(last_name)
 FROM employees
@@ -35,27 +35,27 @@ WHERE last_name LIKE UPPER('&first_letter%' )
 ORDER BY last_name
 
 
-===5.2====
+--===5.2====
 
 SELECT last_name, ROUND(MONTHS_BETWEEN(
        SYSDATE, hire_date)) MONTHS_WORKED
 FROM employees
 ORDER BY months_worked;
 
-====6=====
+--====6=====
 
 SELECT last_name, ROUND(MONTHS_BETWEEN(
        SYSDATE, hire_date)) MONTHS_WORKED
 FROM employees
 ORDER BY months_worked;
 
-====7====
+--====7====
 
 SELECT last_name, LPAD(salary, 15, '$') SALARY
 FROM employees;
 
 
-====8=====
+--====8=====
 
 SELECT last_name, trunc((SYSDATE-hire_date)/7) AS TENURE
 FROM employees
